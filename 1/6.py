d3fcac6e1f337fb956d1f8c12a0783e2bcaca885
Endolite <41879690+Endolite@users.noreply.github.com>
2022-09-12 01:44:49 -0400

    09/12/22 0144

---
+++ 1/6.py
@@ -0,0 +1,12 @@
+def pascal(x):
+  sol = [["" for i in range(x + 1)] for j in range(x + 1)]
+  for i in range(x + 1):
+    for j in range(x + 1):
+      if (j == 0 or j == i):
+        sol[i][j] = 1
+      else:
+        sol[i][j] = sol[i - 1][j - 1] + sol[i - 1][j]
+    for j in range(x + 1):
+      print(sol[i][j], end = " ");
+    print();
+arr = pascal(8)
